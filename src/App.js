a29a1ad0ff71ffdf19431b66c6261387719d38a3
Adel <htwr86@gmail.com>
2019-08-06 13:36:05 +0300

    particles added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import 'tachyons';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation';
 import Logo from './components/Logo';
+import Rank from './components/Rank';
 import ImageLinkForm from './components/ImageLinkForm';
 import './App.css';
+
+const particlesParams = {
+  polygon: {
+      
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='fixed-ns top-0 right-0 bottom-0 left-0 z--1'
+                params={particlesParams} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="f2 white">
+        {'Adel, your rank is...'}
+      </div>
+      <div className="f1 white">
+        {'5'}
+      </div>
+    </div>
+  )
+}
+
+export default Rank;
